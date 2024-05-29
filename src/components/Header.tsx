b74a65d828affd1a5def5f2b0cba666da505d812
augustospr <augusto_spr@hotmail.com>
2024-05-28 23:06:52 -0300

    att

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,34 @@
+import asprLogo from "../assets/aspr-logo.png";
+// import nomeLogo from "../assets/augusto-logo-nome.png";
+
 const Header = () => {
 
   return (
     <>
-      <section className="header">
+      <section id="header" className="header">
         <div className="container">
           <div className="row">
+
             <div className="box-image">
-              <figure>
-                <img src="" alt="" />
-              </figure>
+              <a href="/">
+                <figure>
+                  <img src={asprLogo} alt="logo do nome Augusto Sérgio Pimenta Ramos" />
+                </figure>
+              </a>
             </div>
+
             <div className="box-clickable">
               <div className="box-click">
-                <a href="">Eu</a>
+                <a href="">Tecnologias</a>
               </div>
               <div className="box-click">
-                <a href="">Eu</a>
+                <a href="">Projetos</a>
               </div>
               <div className="box-click">
-                <a href="">Eu</a>
+                <a href=""><button>Say Hello</button></a>
               </div>
             </div>
+
           </div>
         </div>
       </section>
